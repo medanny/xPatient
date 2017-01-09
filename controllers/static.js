851ba4dd00898cb55c1b81d3f5dc20c62fe9c493
root <root@medanny.cloud>
2017-01-09 17:49:11 -0500

    added angular2 client

--- controllers/static.js
+++ controllers/static.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-router.use(express.static(__dirname + '/../assets'));
+router.use(express.static(__dirname + '/../client/dist/'));
 router.use(express.static(__dirname + '/../node_modules'));
 
 router.get('/', function(req, res){
-	res.sendfile('layouts/nutriadmin.html');
+	res.sendfile('client/dist/index.html');
 });
 
 module.exports = router;
